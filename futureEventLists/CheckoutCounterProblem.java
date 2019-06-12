e2789c35c6903b688820e9b6818b3fadcaf928ef
JKNags <johnknagel@gmail.com>
2019-06-12 12:27:22 -0400

    added total times for each operation - dump truck

--- futureEventLists/CheckoutCounterProblem.java
+++ futureEventLists/CheckoutCounterProblem.java
@@ -1,12 +1,18 @@
 package futureEventLists;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class CheckoutCounterProblem {
 
 	// Variables
-	static int[] interArrivalTimes = {4, 5, 2, 8, 3, 7}; //{1, 1, 6, 3, 7, 5, 2, 4, 1};   //...
-	static int[] serviceTimes = {5, 3, 4, 6, 2, 7}; //{4, 2, 5, 4, 1, 5, 4, 1, 4};   //...
+	static int[] interArrivalTimes = {4, 5, 2, 8, 3, 7}; //{1, 1, 6, 3, 7, 5, 2, 4, 1};
+	static int[] serviceTimes = {5, 3, 4, 6, 2, 7}; //{4, 2, 5, 4, 1, 5, 4, 1, 4};
 	static int arrivalIndex = 0, serviceIndex = 0, time = 0, queueLength = 0, serviceLength = 0;
 	static int nextArrivalTime, nextDepartureTime, busyTime, maxQueueLength = 0, timeIncrement;
+	static int customerIdx = 0;
+	static Map<Integer, Integer> custArrivals;
 	static final int endTime = 40, numServers = 1;
 	
 	public static void main(String[] args) {	
@@ -16,6 +22,10 @@ public class CheckoutCounterProblem {
 		serviceLength = 1;  // begin with one customer being serviced
 		nextArrivalTime = interArrivalTimes[arrivalIndex++];
 		nextDepartureTime = serviceTimes[serviceIndex++];
+		
+		int custNum = 1;
+		custArrivals = new HashMap<Integer, Integer>();
+		
 		PrintFEL();
 		//time = 1;
 		
@@ -45,12 +55,16 @@ public class CheckoutCounterProblem {
 				
 				// Check for arrival time
 				if (time == nextArrivalTime) {
+					custNum++;
+					
 					// Server(s) busy
 					if (serviceLength == numServers) {
+						custArrivals.put(custNum, time);
 						queueLength++;
 					} else {
 						serviceLength++;
 					}
+					
 					nextArrivalTime = time + interArrivalTimes[arrivalIndex++];
 				}
 				
@@ -74,6 +88,7 @@ public class CheckoutCounterProblem {
 		output += String.format("%-5s", "T");
 		output += String.format("%-5s", "LQt");
 		output += String.format("%-5s", "LSt");
+		output += String.format("%-15s", "Line");
 		output += String.format("%-22s", "FEL");
 		output += String.format("%-5s", "B");
 		output += String.format("%-5s", "MaxQ");
@@ -82,10 +97,21 @@ public class CheckoutCounterProblem {
 	
 	static void PrintFEL() {
 		String output = "";
+		
+		String line = "";
+	    Iterator it = custArrivals.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        line = "(C" + pair.getKey() + "," + pair.getValue() + "),";
+	        it.remove(); // avoids a ConcurrentModificationException
+	    }
+	    
+	    line = line.length() == 0 ? "" : line.substring(0, line.length()-1);
 	
 		output += String.format("%-5d", time);
 		output += String.format("%-5d", queueLength);
 		output += String.format("%-5d", serviceLength);
+		output += String.format("%-15s", line);
 		if(nextArrivalTime<nextDepartureTime)output += String.format("%-22s", "(A," + nextArrivalTime + "), (D," + nextDepartureTime + ")");
 		else output += String.format("%-22s", "(D," + nextDepartureTime + "), (A," + nextArrivalTime + ")");
 		output += String.format("%-5d", busyTime);
--- futureEventLists/DumpTruckProblem.java
+++ futureEventLists/DumpTruckProblem.java
@@ -14,16 +14,18 @@ public class DumpTruckProblem {
 	static ArrayList<Integer> loadingTrucks = new ArrayList<Integer>();
 	static ArrayList<Integer> scaleQueue = new ArrayList<Integer>();
 	static ArrayList<Integer> scalingTrucks = new ArrayList<Integer>();
+	static ArrayList<Integer> travellingTrucks = new ArrayList<Integer>();
 	static int busyLoader = 0, busyScale = 0;
 	static int timeIncrement;
 	static DumpTruckEventList events;
+	static int[] loaderQueueTotalTimes, loadingTotalTimes, scaleQueueTotalTimes, scalingTotalTimes, travelTotalTimes;
 	static final int numTrucks = 6, numLoaders = 2, numScales = 1, endTime = 100;
 	// 6 Trucks
 	// 2 Loaders
 	// 1 Scale
 	
 	public static void main(String[] args) {
-		PrintHeader();
+		PrintHeaderTable();
 		
 		DumpTruckEvent newEvent;
 		
@@ -35,8 +37,13 @@ public class DumpTruckProblem {
 		events.add(new DumpTruckEvent(DumpTruckEvent.Type.EndLoader, loadingTimes[loadingIdx++], 2));
 		events.add(new DumpTruckEvent(DumpTruckEvent.Type.EndLoader, loadingTimes[loadingIdx++], 3));
 		events.add(new DumpTruckEvent(DumpTruckEvent.Type.EndScale, scaleTimes[scaleIdx++], 1));
-		
-		PrintFEL();
+		loaderQueueTotalTimes = new int[6];
+		loadingTotalTimes = new int[6];
+		scaleQueueTotalTimes = new int[6];
+		scalingTotalTimes = new int[6];
+		travelTotalTimes = new int[6];
+		boolean debug = false;
+		PrintFelTable();
 		
 		// Loop
 		try {
@@ -45,24 +52,51 @@ public class DumpTruckProblem {
 				time += timeIncrement;
 				DumpTruckEvent nextEvent = events.pop();
 				
-				//System.out.println("\tT:"+time+"(+"+timeIncrement+"), E:"+nextEvent);
-			
+				// Ran out of times
+				if ((loadingIdx == loadingTimes.length && nextEvent.getType().equals(DumpTruckEvent.Type.ArriveLoader))
+						|| (scaleIdx == scaleTimes.length && nextEvent.getType().equals(DumpTruckEvent.Type.EndLoader) && scalingTrucks.size() != numScales)
+						|| (travelIdx == travelTimes.length && nextEvent.getType().equals(DumpTruckEvent.Type.EndScale))) {
+					break;
+				}
+				
+				System.out.println("loadingIdx="+loadingIdx+", scaleIdx="+scaleIdx+", travelIdx="+travelIdx);
+				
 				// Set busy time
 				busyLoader += loadingTrucks.size() * timeIncrement;
 				busyScale += scalingTrucks.size() * timeIncrement;
 				
+				// Set total times
+				for (int t : loaderQueue) {
+					loaderQueueTotalTimes[t-1] += timeIncrement;
+				}
+				for(int t : loadingTrucks) {
+					loadingTotalTimes[t-1] += timeIncrement;
+				}
+				for(int t : scaleQueue) {
+					scaleQueueTotalTimes[t-1] += timeIncrement;
+				}
+				for(int t : scalingTrucks) {
+					scalingTotalTimes[t-1] += timeIncrement;
+				}
+				for(int t : travellingTrucks) {
+					travelTotalTimes[t-1] += timeIncrement;
+				}
+				
+				// Respond to event
 				switch (nextEvent.getType()) {
 				
 					// ARRIVE LOADER EVENT
 					case ArriveLoader:
+						travellingTrucks.remove(new Integer(nextEvent.getTruckNum()));
+						
 						if (loadingTrucks.size() == numLoaders) {
 							// Loader(s) full
-							loaderQueue.add(nextEvent.getTruckNum());		System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to loader queue");
+							loaderQueue.add(nextEvent.getTruckNum());		if (debug) System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to loader queue");
 						} else {
 							// Loader(s) available
 							loadingTrucks.add(nextEvent.getTruckNum());
 							newEvent = new DumpTruckEvent(DumpTruckEvent.Type.EndLoader, time + loadingTimes[loadingIdx++], nextEvent.getTruckNum());
-							events.add(newEvent);	System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to loader : " + newEvent);
+							events.add(newEvent);	if (debug) System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to loader : " + newEvent);
 						}
 						break;
 					
@@ -72,12 +106,12 @@ public class DumpTruckProblem {
 						
 						if (scalingTrucks.size() == numScales) {
 							// Scale full
-							scaleQueue.add(nextEvent.getTruckNum());		System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to scale queue");
+							scaleQueue.add(nextEvent.getTruckNum());		if (debug) System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to scale queue");
 						} else {
 							// Scale available
 							scalingTrucks.add(nextEvent.getTruckNum());
 							newEvent = new DumpTruckEvent(DumpTruckEvent.Type.EndScale, time + scaleTimes[scaleIdx++], nextEvent.getTruckNum());
-							events.add(newEvent);	System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to scale : " + newEvent);
+							events.add(newEvent);	if (debug) System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to scale : " + newEvent);
 						}
 						
 						// Pull another truck into Loader
@@ -85,7 +119,7 @@ public class DumpTruckProblem {
 							int toLoaderTruck = loaderQueue.remove(0);
 							loadingTrucks.add(toLoaderTruck);
 							newEvent = new DumpTruckEvent(DumpTruckEvent.Type.EndLoader, time + loadingTimes[loadingIdx++], toLoaderTruck);
-							events.add(newEvent);	System.out.println("\tAdding DT:"+toLoaderTruck+" to loader : " + newEvent);
+							events.add(newEvent);	if (debug) System.out.println("\tAdding DT:"+toLoaderTruck+" to loader : " + newEvent);
 						}
 						break;
 						
@@ -94,45 +128,51 @@ public class DumpTruckProblem {
 						scalingTrucks.remove(new Integer(nextEvent.getTruckNum()));
 						
 						newEvent = new DumpTruckEvent(DumpTruckEvent.Type.ArriveLoader, time + travelTimes[travelIdx++], nextEvent.getTruckNum());
-						events.add(newEvent);	System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to travel : " + newEvent);
+						events.add(newEvent);	if (debug) System.out.println("\tAdding DT:"+nextEvent.getTruckNum()+" to travel : " + newEvent);
 						
 						// Pull another truck into scale
 						if (scaleQueue.size() > 0) {
 							int toScaleTruck = scaleQueue.remove(0);
 							scalingTrucks.add(toScaleTruck);
 							newEvent = new DumpTruckEvent(DumpTruckEvent.Type.EndScale, time + scaleTimes[scaleIdx++], toScaleTruck);
-							events.add(newEvent);	System.out.println("\tAdding DT:"+toScaleTruck+" to scale : " + newEvent);
+							events.add(newEvent);	if (debug) System.out.println("\tAdding DT:"+toScaleTruck+" to scale : " + newEvent);
 						}
-						
+
+						travellingTrucks.add(nextEvent.getTruckNum());
 						break;
 				}
 				
-				PrintFEL();
-				
-				// TODO: prevent ArrayIndexOutOfBoundsException
+				PrintFelTable();
 			}	
 		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
 		}
 		
+		System.out.println("\n\nDT\tLQ\tL\tWQ\tW\tT");
+		for (int t = 0; t < 6; t++) {
+			System.out.println("DT" + (t+1) + "\t" + loaderQueueTotalTimes[t] + "\t"  + loadingTotalTimes[t] + "\t" + scaleQueueTotalTimes[t] + "\t" + scalingTotalTimes[t] + "\t" + travelTotalTimes[t]);
+		}
 	}
 	
 	static void PrintHeader() {
 		String output = "";
-		output += String.format("%-5s", "T");
+		output += String.format("%-5s", "t");
 		output += String.format("%-13s", "LQ");
 		output += String.format("%-10s", "L");
 		output += String.format("%-13s", "WQ");
 		output += String.format("%-10s", "W");
-		//output += String.format("%-13s", "LoadQ");
-		//output += String.format("%-13s", "ScaleQ");
+		output += String.format("%-15s", "T");
 		output += String.format("%-20s", "FEL");
 		output += String.format("%-5s", "BL");
 		output += String.format("%-5s", "BS");
 		System.out.println(output);
 	}
 	
-	static void PrintFEL() {
+	static void PrintHeaderTable() {
+		System.out.println("t\t"+"LQ\t"+"L\t"+"WQ\t"+"W\t"+"T\t"+"FEL\t"+"BL\t"+"BS\t");
+	}
+	
+	static void PrintFel() {
 		String output = "";
 		String[] eventsString = events.toStringList();
 		output += String.format("%-5d", time);
@@ -140,15 +180,33 @@ public class DumpTruckProblem {
 		output += String.format("%-10s", Arrays.toString(loadingTrucks.toArray()));
 		output += String.format("%-13s", Arrays.toString(scaleQueue.toArray()));
 		output += String.format("%-10s", Arrays.toString(scalingTrucks.toArray()));
-		//output += String.format("%-13s", Arrays.toString(loaderQueue.toArray()));
-		//output += String.format("%-13s", Arrays.toString(scaleQueue.toArray()));
+		output += String.format("%-15s", Arrays.toString(travellingTrucks.toArray()));
 		output += String.format("%-20s", eventsString[0]);
 		output += String.format("%-5d", busyLoader);
 		output += String.format("%-5d", busyScale);
+		
 		for (int idx = 1; idx < eventsString.length; idx++) {
-			output += String.format("\n%-51s%-20s", "", eventsString[idx]);
+			output += String.format("\n%-56s%-20s", "", eventsString[idx]);
 		}
+		
 		System.out.println(output);
+	}
+	
+	static void PrintFelTable() {
+		String output = "";
+		String[] eventsString = events.toStringList();
+		System.out.println(time+"\t"
+				+Arrays.toString(loaderQueue.toArray())+"\t"
+				+Arrays.toString(loadingTrucks.toArray())+"\t"
+				+Arrays.toString(scaleQueue.toArray())+"\t"
+				+Arrays.toString(scalingTrucks.toArray())+"\t"
+				+Arrays.toString(travellingTrucks.toArray())+"\t"
+				+eventsString[0]+"\t"
+				+busyLoader+"\t"
+				+busyScale);
+		for (int idx = 1; idx < eventsString.length; idx++) {
+			System.out.println("\t\t\t\t\t\t"+eventsString[idx]);
+		}	
 	}
 
 }
